dd31e87be773dfde8ae88d67afba322986874da4
jkdearnley <jkdearnley@gmail.com>
2019-11-15 12:52:54 +1300

    Added to  fetchAction

--- github-search/src/actions/fetchAction.js
+++ github-search/src/actions/fetchAction.js
@@ -18,3 +18,18 @@ export const receive_error = () => {
         type: "RECEIVE_ERROR"
     };
 };
+
+export const thunk_action_creator = username => {
+    const user = username.replace(/\s/g, "");
+    store.dispatch(fetch_post());
+    return function(dispatch, getState) {
+      return fetch(`https://api.github.com/users/${user}`)
+        .then(data => data.json())
+        .then(data => {
+          if (data.message === "Not Found") {
+            throw new Error("No such user found!!");
+          } else dispatch(receive_post(data));
+        })
+        .catch(err => dispatch(receive_error()));
+    };
+  };
