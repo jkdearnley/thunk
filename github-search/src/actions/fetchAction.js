24c135c2f00a59c2499230732bd23f80c403270c
jkdearnley <jkdearnley@gmail.com>
2019-11-15 12:46:25 +1300

    fetchAction

---
+++ github-search/src/actions/fetchAction.js
@@ -0,0 +1,20 @@
+import store from "../store"
+
+export const fetch_post = () => {
+    return {
+        type: "FETCH_USER"
+    };
+};
+
+export const receive_post = post => {
+    return {
+        type: "FETCHED_USER",
+        data: post
+    };
+};
+
+export const receive_error = () => {
+    return {
+        type: "RECEIVE_ERROR"
+    };
+};
