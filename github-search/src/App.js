e64d2d2f61a812fe3cc9b710473cd3e80774f8ae
jkdearnley <jkdearnley@gmail.com>
2019-11-15 13:18:41 +1300

    Added form app.js

--- github-search/src/App.js
+++ github-search/src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    const username = this.getUsername.value;
+    console.log(username);
+  };
   render() {
     return (
-      <div>
-        <h2>Hello React!</h2>
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="form">
+          <h2 className="title">Enter the Github Username</h2>
+          <input
+            type="text"
+            placeholder="Enter Github Username"
+            required
+            ref={input => (this.getUsername = input)}
+            />
+            <button className="button">Submit</button>
+        </form>
       </div>
     );
   }
