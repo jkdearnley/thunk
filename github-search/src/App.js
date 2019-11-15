eeadf80183b94ba6d61b9e1a675ae3773dd11970
jkdearnley <jkdearnley@gmail.com>
2019-11-15 13:26:11 +1300

    Added UserInfo

--- github-search/src/App.js
+++ github-search/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import UserInfo from "./UserInfo";
+import { thunk_action_creator } from "./actions/fetchAction";
 
 class App extends Component {
   handleSubmit = e => {
     e.preventDefault();
     const username = this.getUsername.value;
-    console.log(username);
+    this.props.dispatch(thunk_action_creator(username));
+    this.getUsername.value = "";
   };
   render() {
     console.log(this.props.data);
@@ -21,6 +24,13 @@ class App extends Component {
           />
           <button className="button">Submit</button>
         </form>
+        {this.props.data.isFetching ? <h3>Loading...</h3> : null}
+        {this.props.data.isError ? (
+          <h3 className="error">No such User exists.</h3>
+        ) : null}
+        {Object.keys(this.props.data.userData).length > 0 ? (
+          <UserInfo user={this.props.data.userData} />
+        ) : null}
       </div>
     );
   }
