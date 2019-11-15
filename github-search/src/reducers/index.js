a54a6d6509f027a1704301e579e1580eb5cbcfd8
jkdearnley <jkdearnley@gmail.com>
2019-11-15 13:12:40 +1300

    Extended to reducer index

--- github-search/src/reducers/index.js
+++ github-search/src/reducers/index.js
@@ -5,7 +5,27 @@ const initialState = {
 };
 
 const asyncReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case "FETCH_USER":
+            return Object.assign({}, state, {
+                isFetching: true,
+                userData: {},
+                isError: false
+            });
+        case "FETCHED_USER":
+            return Object.assign({}, state, {
+                userData: action.data,
+                isFetching: false,
+                isError: false
+            });
+        case "RECEIVE_ERROR":
+            return Object.assign({}, state, {
+                isError: true,
+                isFetching: false
+            });
+            default:
+                return state;
+    } 
 };
 
 export default asyncReducer;
